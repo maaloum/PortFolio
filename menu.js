cc28a748224ce29898dff942a776c7c56dfee104
Maaloum Ely Cheick <Maaloumali1@gmail.com>
2022-07-20 12:54:18 +0300

    add mobile menu

--- menu.js
+++ menu.js
@@ -1,26 +1,18 @@
-function mobileMenu(){
-  var container = document.querySelector(".container");
-  var nav = document.querySelector("nav");
-  var span = document.createElement('span');
-  span.innerHTML += "<span>&times;</span>";
-  span.classList.add("close");
+const icon = document.querySelector('#icon');
+icon.addEventListener('click', () => {
+  const container = document.querySelector('.container');
+  const nav = document.querySelector('nav');
+  const span = document.createElement('span');
+  span.innerHTML += '<span>&times;</span>';
+  span.classList.add('close');
   nav.appendChild(span);
-  var menu = document.querySelector(".menu");
-  var navMenu = document.querySelector(".nav-menu");
-  var navLink = document.querySelector(".nav-link");
-
-  if(nav.style.display === 'inline-block'){
-    nav.style.display = "none";
-  } else{
-    nav.style.display = "block";
+  if (nav.style.display === 'inline-block') {
+    nav.style.display = 'none';
+  } else {
+    nav.style.display = 'block';
     nav.classList.add('nav-style');
-    span.addEventListener("click", () => {
+    span.addEventListener('click', () => {
       container.removeChild(nav);
     });
-    document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-      container.removeChild(nav);
-    }));
-
   }
-
-}
+});
